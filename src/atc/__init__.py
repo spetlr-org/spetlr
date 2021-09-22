1a459cf72eba53ac7063a119e331e941fe0f5990
Simon Heisterkamp <simon@heisterkamp.dk>
2021-09-22 20:37:24 +0200

    fixed init tests

--- src/atc/__init__.py
+++ src/atc/__init__.py
@@ -4,4 +4,6 @@ See https://github.com/atc-net/atc-dataplatform for details
 """
 __version__ = "0.1.7"
 
-import atc.Spark as Spark
+from atc import spark
+from atc import sql
+from atc.config_master.config_master import ConfigMaster

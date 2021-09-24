91af25e7acda059568beca73a81de7424c1e1393
Simon Heisterkamp <simon@heisterkamp.dk>
2021-09-24 13:59:43 +0200

    incremented version

--- src/atc/__init__.py
+++ src/atc/__init__.py
@@ -2,7 +2,7 @@
 A common set of python libraries for DataBricks.
 See https://github.com/atc-net/atc-dataplatform for details
 """
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 
 from atc import spark
 from atc import sql

6e228860e5805029aad4955b023df8719095d859
Simon Heisterkamp <simon@heisterkamp.dk>
2021-09-24 08:30:34 +0200

    add spark to unittest pipeleine

--- tests/test_spark.py
+++ tests/test_spark.py
@@ -5,7 +5,9 @@ import atc
 
 class TestSparkImport(unittest.TestCase):
     def test_spark(self):
-        Spark = atc.spark
+        spark = atc.spark.Spark.master("local[*]").get()
+        df = spark.sql("select 42")
+        self.assertEqual(42, df.collect()[0][0])
 
 
 if __name__ == "__main__":

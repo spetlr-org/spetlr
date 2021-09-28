30b8bdb9a951af43d5ab989a6c3a10029e50d9d2
Lau Johansson <lau_johansson@hotmail.com>
2021-09-28 13:02:24 +0200

    Remove unused imports

--- src/atc/functions.py
+++ src/atc/functions.py
@@ -6,55 +6,45 @@ from atc.spark import Spark
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
 import uuid as _uuid
-import pyspark.sql.functions as F
-from pathlib import Path
 
 # Pyspark uuid function implemented as recommended here
 # https://stackoverflow.com/questions/49785108/spark-streaming-with-python-how-to-add-a-uuid-column/50095755
 uuid = udf(lambda: str(_uuid.uuid4()), StringType()).asNondeterministic()  # noqa
 
-
-def init_dbutils(spark):
+def init_dbutils():
     try:
         from pyspark.dbutils import DBUtils
-
-        dbutils = DBUtils(spark)
+        dbutils = DBUtils(Spark.get())
     except ImportError:
         import IPython
-
         dbutils = IPython.get_ipython().user_ns["dbutils"]
     return dbutils
 
+class NoTableException(Exception):
+    pass
 
-def drop_table_cascade(DataBaseName, TableName):
 
-    # Check if table exists
-    if Spark.get()._jsparkSession.catalog().tableExists(DataBaseName, TableName):
+def drop_table_cascade(DBDotTableName: str ) -> None:
+    """
+    drop_table_cascade drops a databricks database table and remove the files permanently
 
-        #### GET PATH - Idea 1 ###
-        # #Collect file path
-        # file_name = spark.read.table(f"{DataBaseName}.{TableName}").select(F.input_file_name()).take(1)
-        # #Collect parent
-        # table_path=str(Path(file_name).parent)
-        ##########################
+    :param p1: db.tablename
+    
+    """
 
-        #### GET PATH - Idea 2 ###
-        table_path = str(
-            Spark.get()
-            .sql(f"DESCRIBE DETAIL {DataBaseName}.{TableName}")
-            .collect()[0]["location"]
-        )
-        ##########################
+    #Check if table exists 
+    if Spark.get()._jsparkSession.catalog().tableExists(DBDotTableName):
 
+        # Get table path 
+        table_path=str(Spark.get().sql(f"DESCRIBE DETAIL {DBDotTableName}").collect()[0]["location"])
+        
         if table_path is None:
-            raise Exception(f"Table path is NONE.")
-
-        Spark.get().sql(
-            f"DROP TABLE IF EXISTS {DataBaseName}.{TableName}"
-        )  # Remove table
-        init_dbutils(Spark.get()).fs.rm(
-            table_path, recurse=True
-        )  # Remove files associated with table
+            raise NoTableException(f"Table path is NONE.")
+        
+        # Remove table 
+        Spark.get().sql(f"DROP TABLE IF EXISTS {DBDotTableName}")
+        # Remove files associated with table        
+        init_dbutils().fs.rm(table_path, recurse=True) 
 
     else:
-        raise Exception(f"The table {DataBaseName}.{TableName} not found.")
+        raise NoTableException(f"The table {DBDotTableName} not found. Rembember syntax db.tablename.")

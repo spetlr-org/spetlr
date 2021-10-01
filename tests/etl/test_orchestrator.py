9ca92c5c9959d452a18b1dc9a3701cf989f8befe
Christian Helle <christian.helle@outlook.com>
2021-10-01 14:43:02 +0200

    Implement Orchestrator tests

---
+++ tests/etl/test_orchestrator.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest.mock import MagicMock
+
+from atc import Orchestrator, OrchestratorFactory
+
+
+class OrchestratorTests(unittest.TestCase):
+
+    def test_execute_invokes_extractor_read(self):
+        sut = self._create_sut()
+        sut.execute()
+        sut.extractor.read.assert_called_once()
+
+    def test_execute_invokes_transformer_process(self):
+        sut = self._create_sut()
+        sut.execute()
+        sut.transformer.process.assert_called_once()
+
+    def test_execute_invokes_loader_save(self):
+        sut = self._create_sut()
+        sut.execute()
+        sut.loader.save.assert_called_once()
+
+    @staticmethod
+    def _create_sut() -> Orchestrator:
+        return OrchestratorFactory.create(MagicMock(),
+                                          MagicMock(),
+                                          MagicMock())

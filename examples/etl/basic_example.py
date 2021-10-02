b5c278319165df9e46d55ab0110b5f9346173280
Christian Helle <christian.helle@outlook.com>
2021-10-02 15:30:26 +0200

    Create sample code for basic ETL

---
+++ examples/etl/basic_example.py
@@ -0,0 +1,48 @@
+import pyspark.sql.functions as f
+from pyspark.sql import DataFrame
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+from atc.etl import Extractor, Transformer, Loader, OrchestratorFactory, MultiInputTransformer
+from atc.spark import Spark
+
+
+class GuitarExtractor(Extractor):
+    def read(self) -> DataFrame:
+        return Spark.get().createDataFrame(
+            Spark.get().sparkContext.parallelize([
+                ('1', 'Fender', 'Telecaster', '1950'),
+                ('2', 'Gibson', 'Les Paul', '1959'),
+                ('3', 'Ibanez', 'RG', '1987')
+            ]),
+            StructType([
+                StructField('id', StringType()),
+                StructField('brand', StringType()),
+                StructField('model', StringType()),
+                StructField('year', StringType()),
+            ]))
+
+
+class BasicTransformer(Transformer):
+    def process(self, df: DataFrame) -> DataFrame:
+        print('Current DataFrame schema')
+        df.printSchema()
+
+        df = df.withColumn('id', f.col('id').cast(IntegerType()))
+        df = df.withColumn('year', f.col('year').cast(IntegerType()))
+
+        print('New DataFrame schema')
+        df.printSchema()
+        return df
+
+
+class NoopLoader(Loader):
+    def save(self, df: DataFrame) -> DataFrame:
+        df.write.format('noop').mode('overwrite').save()
+        return df
+
+
+print('ETL Orchestrator using a single simple transformer')
+etl = OrchestratorFactory.create(GuitarExtractor(), BasicTransformer(), NoopLoader())
+result = etl.execute()
+result.printSchema()
+result.show()

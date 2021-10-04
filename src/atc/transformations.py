9276dad848d1c1addf147219ac43827e08ad2dc9
Jeppe Blixenkrone-Møller <jbl@delegate.dk>
2021-10-04 10:03:28 +0200

    Update transforamtion functions with running black

--- src/atc/transformations.py
+++ src/atc/transformations.py
@@ -4,7 +4,15 @@ from pyspark.sql import Window
 
 from typing import List
 
-def join_time_series_dataframes(dfPrimary: DataFrame, dfSecondary: DataFrame, startTimeColumnName: str, endTimeColumnName: str, idColumns: List[str], stateColumn: str) -> DataFrame:
+
+def join_time_series_dataframes(
+    dfPrimary: DataFrame,
+    dfSecondary: DataFrame,
+    startTimeColumnName: str,
+    endTimeColumnName: str,
+    idColumns: List[str],
+    stateColumn: str,
+) -> DataFrame:
 
     # Rename columns to differantiate the two dataframes
     for column in dfPrimary.columns:
@@ -12,7 +20,7 @@ def join_time_series_dataframes(dfPrimary: DataFrame, dfSecondary: DataFrame, st
 
     for column in dfSecondary.columns:
         dfSecondary = dfSecondary.withColumnRenamed(column, f"dfSec_{column}")
-    
+
     # Create join expection
     idMatchString = " AND ".join([f"dfPri_{column} = dfSec_{column}" for column in idColumns])
     timeMatchString = f"((dfSec_{startTimeColumnName} >= dfPri_{startTimeColumnName} AND dfSec_{startTimeColumnName} < dfPri_{endTimeColumnName}) OR (dfSec_{endTimeColumnName} >= dfPri_{startTimeColumnName} AND dfSec_{endTimeColumnName} < dfPri_{endTimeColumnName}))"
@@ -23,51 +31,109 @@ def join_time_series_dataframes(dfPrimary: DataFrame, dfSecondary: DataFrame, st
 
     # Select data after join to remove duplicated id columns
     df_join = df_join.select(
-        [F.col(f"dfPri_{column}").alias(column) for column in idColumns] +
-        [f"dfPri_{column}" for column in [startTimeColumnName, endTimeColumnName, stateColumn]] +
-        [f"dfSec_{column}" for column in [startTimeColumnName, endTimeColumnName, stateColumn]]
+        [F.col(f"dfPri_{column}").alias(column) for column in idColumns]
+        + [f"dfPri_{column}" for column in [startTimeColumnName, endTimeColumnName, stateColumn]]
+        + [f"dfSec_{column}" for column in [startTimeColumnName, endTimeColumnName, stateColumn]]
     )
 
     # Create state id to use for groupings becouse value can change back to same value
     window = Window.partitionBy(idColumns).orderBy(f"dfPri_{startTimeColumnName}")
-    df_join = df_join.withColumn("dfPri_StateGroupId", F.sum((F.col(f"dfPri_{stateColumn}") != F.lag(F.col(f"dfPri_{stateColumn}")).over(window)).cast("int")).over(window))
+    df_join = df_join.withColumn(
+        "dfPri_StateGroupId",
+        F.sum(
+            (
+                F.col(f"dfPri_{stateColumn}") != F.lag(F.col(f"dfPri_{stateColumn}")).over(window)
+            ).cast("int")
+        ).over(window),
+    )
     df_join = df_join.fillna({"dfPri_StateGroupId": 0})
 
     # Fix start and end timestamps when multiple states in secondary dataframe match primary dataframe
     window = Window.partitionBy(idColumns + ["dfPri_StateGroupId"]).orderBy(f"dfPri_{startTimeColumnName}", f"dfSec_{startTimeColumnName}")
-    df_join = df_join.withColumn(f"dfPri_{endTimeColumnName}", F.when((F.col(f"dfPri_{endTimeColumnName}") == F.lead(F.col(f"dfPri_{endTimeColumnName}")).over(window)), F.col(f"dfSec_{endTimeColumnName}")).otherwise(F.col(f"dfPri_{endTimeColumnName}")))
-    df_join = df_join.withColumn(f"dfPri_{startTimeColumnName}", F.when((F.col(f"dfPri_{startTimeColumnName}") == F.lag(F.col(f"dfPri_{startTimeColumnName}")).over(window)), F.lag(F.col(f"dfSec_{endTimeColumnName}")).over(window)).otherwise(F.col(f"dfPri_{startTimeColumnName}")))
+    df_join = df_join.withColumn(
+        f"dfPri_{endTimeColumnName}",
+        F.when(
+            (F.col(f"dfPri_{endTimeColumnName}") == F.lead(F.col(f"dfPri_{endTimeColumnName}")).over(window)),
+            F.col(f"dfSec_{endTimeColumnName}"),
+        ).otherwise(F.col(f"dfPri_{endTimeColumnName}")),
+    )
+    df_join = df_join.withColumn(
+        f"dfPri_{startTimeColumnName}",
+        F.when(
+            (F.col(f"dfPri_{startTimeColumnName}") == F.lag(F.col(f"dfPri_{startTimeColumnName}")).over(window)),
+            F.lag(F.col(f"dfSec_{endTimeColumnName}")).over(window),
+        ).otherwise(F.col(f"dfPri_{startTimeColumnName}")),
+    )
 
-    # Generating three time segements, one with the state from first the primary dataframe, one with the state from the secondary dataframe and one with the rest of primary dataframe
+    # Generating three time segements, one with the state from the primary dataframe, one with the state from the secondary dataframe and one with the rest of primary dataframe.
     # Correcting the end timestamps acording to if there are a matching element in seconday dataframe
-    df_join = df_join.withColumn("StartTimeDfPri1", F.col(f"dfPri_{startTimeColumnName}"))
-    df_join = df_join.withColumn("EndTimeDfPri1", F.when(F.col(f"dfSec_{stateColumn}").isNotNull(), F.col(f"dfSec_{startTimeColumnName}")).otherwise(F.col(f"dfPri_{endTimeColumnName}")))
+    df_join = df_join.withColumn(
+        "StartTimeDfPri1", F.col(f"dfPri_{startTimeColumnName}")
+    )
+    df_join = df_join.withColumn(
+        "EndTimeDfPri1",
+        F.when(
+            F.col(f"dfSec_{stateColumn}").isNotNull(),
+            F.col(f"dfSec_{startTimeColumnName}"),
+        ).otherwise(F.col(f"dfPri_{endTimeColumnName}")),
+    )
 
-    df_join = df_join.withColumn("StartTimeDfSec", F.col(f"dfSec_{startTimeColumnName}"))
+    df_join = df_join.withColumn(
+        "StartTimeDfSec", F.col(f"dfSec_{startTimeColumnName}")
+    )
     df_join = df_join.withColumn("EndTimeDfSec", F.col(f"dfSec_{endTimeColumnName}"))
 
     df_join = df_join.withColumn("StartTimeDfPri2", F.col(f"dfSec_{endTimeColumnName}"))
-    df_join = df_join.withColumn("EndTimeDfPri2", F.when(F.col(f"dfSec_{stateColumn}").isNotNull(), F.col(f"dfPri_{endTimeColumnName}")).otherwise(None))
+    df_join = df_join.withColumn(
+        "EndTimeDfPri2",
+        F.when(
+            F.col(f"dfSec_{stateColumn}").isNotNull(),
+            F.col(f"dfPri_{endTimeColumnName}"),
+        ).otherwise(None),
+    )
 
     # Collecting segments of timestamps into lists
-    df_join = df_join.withColumn("DfPri1", F.when((F.col("StartTimeDfPri1") < F.col("EndTimeDfPri1")), F.array(F.col("StartTimeDfPri1"), F.col("EndTimeDfPri1"))).otherwise(F.array(F.lit(None), F.lit(None))))
-    df_join = df_join.withColumn("DfSec", F.when((F.col("StartTimeDfSec") < F.col("EndTimeDfSec")), F.array(F.col("StartTimeDfSec"), F.col("EndTimeDfSec"))).otherwise(F.array(F.lit(None), F.lit(None))))
-    df_join = df_join.withColumn("DfPri2", F.when((F.col("StartTimeDfPri2") < F.col("EndTimeDfPri2")), F.array(F.col("StartTimeDfPri2"), F.col("EndTimeDfPri2"))).otherwise(F.array(F.lit(None), F.lit(None))))
+    df_join = df_join.withColumn(
+        "DfPri1",
+        F.when(
+            F.col("StartTimeDfPri1") < F.col("EndTimeDfPri1"),
+            F.array(F.col("StartTimeDfPri1"), F.col("EndTimeDfPri1")),
+        ).otherwise(F.array(F.lit(None), F.lit(None))),
+    )
+    df_join = df_join.withColumn(
+        "DfSec",
+        F.when(
+            F.col("StartTimeDfSec") < F.col("EndTimeDfSec"),
+            F.array(F.col("StartTimeDfSec"), F.col("EndTimeDfSec")),
+        ).otherwise(F.array(F.lit(None), F.lit(None))),
+    )
+    df_join = df_join.withColumn(
+        "DfPri2",
+        F.when(
+            F.col("StartTimeDfPri2") < F.col("EndTimeDfPri2"),
+            F.array(F.col("StartTimeDfPri2"), F.col("EndTimeDfPri2")),
+        ).otherwise(F.array(F.lit(None), F.lit(None))),
+    )
 
     # Unpivot dataframe to move timestamp segemnts from columns to rows
-    unpivot_columnn_select = idColumns + [f"dfPri_{stateColumn}"] + [f"dfSec_{stateColumn}"] + ["stack(3, 'DfPri1', DfPri1, 'DfSec', DfSec, 'DfPri2', DfPri2) as (type, data)"]
+    unpivot_columnn_select = (
+        idColumns + [f"dfPri_{stateColumn}", f"dfSec_{stateColumn}", "stack(3, 'DfPri1', DfPri1, 'DfSec', DfSec, 'DfPri2', DfPri2) as (type, data)"]
+    )
     df_join_unpivot = df_join.selectExpr(*unpivot_columnn_select)
 
     # Extract timestamp from unpivot data column
     df_join_unpivot = df_join_unpivot.withColumn(startTimeColumnName, F.col("data")[0])
     df_join_unpivot = df_join_unpivot.withColumn(endTimeColumnName, F.col("data")[1])
     df_join_unpivot = df_join_unpivot.drop("data")
-    
+
     # Remove rows where start time and end time is null
-    df_join_unpivot = df_join_unpivot.where(F.col(startTimeColumnName).isNotNull() | F.col(endTimeColumnName).isNotNull())
+    df_join_unpivot = df_join_unpivot.where(
+        F.col(startTimeColumnName).isNotNull() | F.col(endTimeColumnName).isNotNull()
+    )
 
     # Define stateColumn from either df primary or df seconday
-    df_join_unpivot = df_join_unpivot.withColumn("tempStateColumn", 
+    df_join_unpivot = df_join_unpivot.withColumn(
+        "tempStateColumn",
         F.when(F.col("type") == "DfPri1", F.col(f"dfPri_{stateColumn}"))
         .when(F.col("type") == "DfSec", F.col(f"dfSec_{stateColumn}"))
         .when(F.col("type") == "DfPri2", F.col(f"dfPri_{stateColumn}"))
@@ -75,7 +141,9 @@ def join_time_series_dataframes(dfPrimary: DataFrame, dfSecondary: DataFrame, st
     )
     df_join_unpivot = df_join_unpivot.drop(f"dfPri_{stateColumn}", f"dfSec_{stateColumn}", "type")
     df_join_unpivot = df_join_unpivot.withColumnRenamed("tempStateColumn", stateColumn)
-    
-    df_join_unpivot = df_join_unpivot.dropDuplicates(subset=[startTimeColumnName, endTimeColumnName])
+
+    df_join_unpivot = df_join_unpivot.dropDuplicates(
+        subset=[startTimeColumnName, endTimeColumnName]
+    )
 
     return df_join_unpivot
--- tests/test_transformations.py
+++ tests/test_transformations.py
@@ -24,26 +24,26 @@ class TestFunctions(unittest.TestCase):
         ]
 
         df2Data = [
-            (1, datetime(year=2020, month=1, day=1, hour=6),  datetime(year=2020, month=1, day=1, hour=8), "state3"),
+            (1, datetime(year=2020, month=1, day=1, hour=6),  datetime(year=2020, month=1, day=1, hour=8),  "state3"),
             (1, datetime(year=2020, month=1, day=1, hour=10), datetime(year=2020, month=1, day=1, hour=12), "state3"),
-            (1, datetime(year=2020, month=1, day=1, hour=20), datetime(year=2020, month=1, day=2, hour=4), "state4"),
-            (1, datetime(year=2020, month=1, day=2, hour=6),  datetime(year=2020, month=1, day=2, hour=8), "state3"),
+            (1, datetime(year=2020, month=1, day=1, hour=20), datetime(year=2020, month=1, day=2, hour=4),  "state4"),
+            (1, datetime(year=2020, month=1, day=2, hour=6),  datetime(year=2020, month=1, day=2, hour=8),  "state3"),
             (1, datetime(year=2020, month=1, day=2, hour=10), datetime(year=2020, month=1, day=2, hour=12), "state3"),
         ]
 
         dfExpectedData = [
-            (1, datetime(year=2020, month=1, day=1, hour=0),  datetime(year=2020, month=1, day=1, hour=6), "state1"),
-            (1, datetime(year=2020, month=1, day=1, hour=6),  datetime(year=2020, month=1, day=1, hour=8), "state3"),
+            (1, datetime(year=2020, month=1, day=1, hour=0),  datetime(year=2020, month=1, day=1, hour=6),  "state1"),
+            (1, datetime(year=2020, month=1, day=1, hour=6),  datetime(year=2020, month=1, day=1, hour=8),  "state3"),
             (1, datetime(year=2020, month=1, day=1, hour=8),  datetime(year=2020, month=1, day=1, hour=10), "state1"),
             (1, datetime(year=2020, month=1, day=1, hour=10), datetime(year=2020, month=1, day=1, hour=12), "state3"),
             (1, datetime(year=2020, month=1, day=1, hour=12), datetime(year=2020, month=1, day=1, hour=20), "state1"),
-            (1, datetime(year=2020, month=1, day=1, hour=20), datetime(year=2020, month=1, day=2, hour=4), "state4"),
-            (1, datetime(year=2020, month=1, day=2, hour=4),  datetime(year=2020, month=1, day=2, hour=6), "state2"),
-            (1, datetime(year=2020, month=1, day=2, hour=6),  datetime(year=2020, month=1, day=2, hour=8), "state3"),
+            (1, datetime(year=2020, month=1, day=1, hour=20), datetime(year=2020, month=1, day=2, hour=4),  "state4"),
+            (1, datetime(year=2020, month=1, day=2, hour=4),  datetime(year=2020, month=1, day=2, hour=6),  "state2"),
+            (1, datetime(year=2020, month=1, day=2, hour=6),  datetime(year=2020, month=1, day=2, hour=8),  "state3"),
             (1, datetime(year=2020, month=1, day=2, hour=8),  datetime(year=2020, month=1, day=2, hour=10), "state2"),
             (1, datetime(year=2020, month=1, day=2, hour=10), datetime(year=2020, month=1, day=2, hour=12), "state3"),
-            (1, datetime(year=2020, month=1, day=2, hour=12), datetime(year=2020, month=1, day=3, hour=0), "state2"),
-            (1, datetime(year=2020, month=1, day=3, hour=0),  datetime(year=2020, month=1, day=4, hour=0), "state1")
+            (1, datetime(year=2020, month=1, day=2, hour=12), datetime(year=2020, month=1, day=3, hour=0),  "state2"),
+            (1, datetime(year=2020, month=1, day=3, hour=0),  datetime(year=2020, month=1, day=4, hour=0),  "state1")
         ]
 
         # Construct dataframes
@@ -51,14 +51,19 @@ class TestFunctions(unittest.TestCase):
         df2 = spark.createDataFrame(df2Data, dfSchema)
         dfExpected = spark.createDataFrame(dfExpectedData, dfSchema)
 
-        dfReturn = join_time_series_dataframes(dfPrimary=df1,
-                                                  dfSecondary=df2,
-                                                  startTimeColumnName="StartTimestamp",
-                                                  endTimeColumnName="EndTimestamp",
-                                                  idColumns=["Id"],
-                                                  stateColumn="State")
+        dfReturn = join_time_series_dataframes(
+            dfPrimary=df1,
+            dfSecondary=df2,
+            startTimeColumnName="StartTimestamp",
+            endTimeColumnName="EndTimestamp",
+            idColumns=["Id"],
+            stateColumn="State",
+        )
 
-        self.assertEqual(dfExpected.orderBy("StartTimestamp").collect(), dfReturn.orderBy("StartTimestamp").collect())
+        self.assertEqual(
+            dfExpected.orderBy("StartTimestamp").collect(),
+            dfReturn.orderBy("StartTimestamp").collect(),
+        )
 
 if __name__ == "__main__":
     unittest.main()

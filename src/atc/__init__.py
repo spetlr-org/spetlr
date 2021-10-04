a51c68a4a33aa031f3940a742f7e0fed857ab188
Jeppe Blixenkrone-Møller <jbl@delegate.dk>
2021-10-04 17:27:55 +0200

    Change version back to 0.2.x

--- src/atc/__init__.py
+++ src/atc/__init__.py
@@ -3,7 +3,7 @@ A common set of python libraries for DataBricks.
 See https://github.com/atc-net/atc-dataplatform for details
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.x"
 
 from atc import spark
 from atc import sql

55218114f99ac81a31c3e4b70b00921708c033f1
Lau Johansson <lau_johansson@hotmail.com>
2022-03-22 15:12:19 +0100

    add sh file pyodbc

--- .github/deploy/steps/05-Pyodbc-Driver.ps1
+++ .github/deploy/steps/05-Pyodbc-Driver.ps1
@@ -1,7 +1,7 @@
 Write-Host "Setting up pyodbc driver" -ForegroundColor DarkYellow
 dbfs mkdirs dbfs:/databricks/drivers
 
-New-Item -Path "$PSScriptRoot/../drivers" -ItemType Directory -Force
+New-Item -Path "$PSScriptRoot/../drivers" -ItemType Directory -Force | Out-Null 
 $odbcSource="https://packages.microsoft.com/ubuntu/20.04/prod/pool/main/m/msodbcsql17/msodbcsql17_17.7.2.1-1_amd64.deb"
 $odbcSaveLocation="$PSScriptRoot/../drivers/msodbcsql17_17.7.2.1-1_amd64.deb"
 

76bab7992c3ac21a8dc4906ddda4689b382de5ca
Simon Heisterkamp <simon@heisterkamp.dk>
2022-03-24 11:54:31 +0100

    table configurator

---
+++ src/atc/config_master/table_configurator.py
@@ -0,0 +1,196 @@
+import importlib.resources
+import json
+import uuid
+from pathlib import Path
+from typing import TypedDict, Union, Dict
+
+import yaml
+
+from atc.config_master.exceptions import UnknownShapeException
+from atc.singleton import Singleton
+
+
+class TableAlias(TypedDict):
+    alias: str
+
+
+class TableDetails(TypedDict):
+    name: str
+    path: str
+    format: str
+    date: str
+
+
+class TableRelDbg(TypedDict):
+    release: Union[TableDetails, TableAlias]
+    debug: Union[TableDetails, TableAlias]
+
+
+TableDetails_keys = set(TableDetails.__annotations__.keys())
+TableAlias_keys = set(TableAlias.__annotations__.keys())
+TableRelDbg_keys = set(TableRelDbg.__annotations__.keys())
+
+
+class TableConfigurator(metaclass=Singleton):
+    unique_id: str
+    table_names: Dict[str, Union[TableDetails, TableAlias, TableRelDbg]]
+    table_arguments: Dict[str, str]
+
+    def __init__(self, resource_path: str = None):
+        self.unique_id = uuid.uuid4().hex
+        self.resource_paths = set()
+        self.table_names = dict()
+
+        if resource_path:
+            self.add_resource_path(resource_path)
+
+        self.__reset()
+
+    def add_resource_path(self, resource_path: str) -> None:
+        self.resource_paths.add(resource_path)
+
+    def __reset(self, debug: bool = False, **kwargs) -> None:
+        self._is_debug = debug
+        self.table_arguments = dict()
+        self.table_arguments["MNT"] = "tmp" if debug else "mnt"
+        self.table_arguments["ID"] = f"__{self.unique_id}" if debug else ""
+        self.table_arguments.update(kwargs)
+
+        self.table_names = dict()
+        for resource_path in self.resource_paths:
+            for file_name in importlib.resources.contents(resource_path):
+                if Path(file_name).suffix in [".json", ".yaml", ".yml"]:
+                    with importlib.resources.path(
+                        resource_path, file_name
+                    ) as file_path:
+                        with open(file_path) as file:
+                            update = (
+                                json.load(file)
+                                if file_name.endswith(".json")
+                                else yaml.load(file, Loader=yaml.FullLoader)
+                            )
+
+                            for key, value in update:
+                                if self.__is_known_shape(value):
+                                    self.table_names[key] = value
+                                else:
+                                    raise UnknownShapeException(
+                                        f"Object {key} in file {file_path}"
+                                        f" has unexpected shape."
+                                    )
+
+        for key in self.table_names.keys():
+            self.__resolve_key(key)
+
+    def __is_TableDetails_shape(self, value):
+        return set(value.keys()).issubset(TableDetails_keys)
+
+    def __is_TableAlias_shape(self, value):
+        return set(value.keys()).issubset(TableAlias_keys)
+
+    def __is_TableRelDbg_shape(self, value):
+        return set(value.keys()).issubset(TableRelDbg_keys)
+
+    def __is_known_shape(self, value):
+        return (
+            self.__is_TableDetails_shape(value)
+            or self.__is_TableAlias_shape(value)
+            or self.__is_TableRelDbg_shape(value)
+        )
+
+    def __resolve_key(self, key: str):
+        # Handle the case of differentiated release and debug tables
+        if self.__is_TableRelDbg_shape(self.table_names[key]):
+            value: TableRelDbg = self.table_names[key]
+            if self._is_debug:
+                self.table_names[key] = value["debug"]
+            else:
+                self.table_names[key] = value["release"]
+
+        # carry out string substitutions in name and path
+        if self.__is_TableDetails_shape(self.table_names[key]):
+            value: TableDetails = self.table_names[key]
+            self.table_names[key]["name"] = value["name"].format(**self.table_arguments)
+            self.table_names[key]["path"] = value["path"].format(**self.table_arguments)
+
+    def __get_name_entry(self, table_id: str):
+        entry = self.table_names[table_id]
+        while "alias" in entry:  # allow alias of alias
+            entry = self.table_names[entry["alias"]]
+        return entry
+
+    def reset(self, debug: bool = False, **kwargs):
+        """
+        Resets table names and table SQL. Enables or disables debug mode (used for unit tests and integration tests).
+        :param debug: False -> release tables, True -> debug tables.
+        :param kwargs: additional keys to be substituted in names and paths
+        """
+        self.__reset(debug, **kwargs)
+
+    def is_debug(self):
+        """
+        Return True if table names and table SQL specify debug table, False if release tables
+        """
+        return self._is_debug
+
+    def get_unique_id_length(self):
+        """
+        Return the character length of the UUID identifier inserted into names with the {ID} tag
+        """
+        return len(self.unique_id)
+
+    def register(self, key: str, value: Union[TableDetails, TableAlias, TableRelDbg]):
+        """
+        Register a new table.
+        """
+        self.table_names[key] = value
+        self.__resolve_key(key)
+
+    def table_property(
+        self, table_id: str, property_name: str, default_value: str = None
+    ):
+        """
+        Return the table property (e.g. name, path, format, etc.) for the specified table id.
+        :param table_id: Table id in the .json or .yaml files.
+        :param property_name: Name of the property to read (e.g. "name", "path", etc.)
+        :param default_value: Optional default value of the property if the property is missing.
+        :return: str: property value
+        """
+        property_value = self.__get_name_entry(table_id).get(
+            property_name, default_value
+        )
+        if property_value is None:
+            raise ValueError(
+                f"property '{property_name}' for table identifier '{table_id}' is empty"
+            )
+        return property_value
+
+    def table_name(self, table_id: str):
+        """
+        Return the table name for the specified table id.
+        :param table_id: Table id in the .json or .yaml files.
+        :return: str: table name
+        """
+        return self.table_property(table_id, "name")
+
+    def table_path(self, table_id: str):
+        """
+        Return the table path for the specified table id.
+        :param table_id: Table id in the .json or .yaml files.
+        :return: str: table path
+        """
+        return self.table_property(table_id, "path")
+
+    def get_all_details(self):
+        table_objects = {}
+        for key in self.table_names.keys():
+            name = self.table_property(key, "name", "")
+            if name:
+                table_objects[key] = name
+                table_objects[key + "_name"] = name
+
+            path = self.table_property(key, "path", "")
+            if path:
+                table_objects[key + "_path"] = path
+
+        return table_objects

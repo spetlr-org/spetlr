e76d171350426e7ddde2515d7e454ea3c2453f31
Simon Heisterkamp <simon@heisterkamp.dk>
2022-08-03 08:05:57 +0000

    revert earlier fix

--- .github/deploy/steps/99-SetupMounts.ps1
+++ .github/deploy/steps/99-SetupMounts.ps1
@@ -6,6 +6,7 @@ Push-Location -Path $srcDir
 pip install dbx
 
 dbx configure
+copy "$srcDir/.github/submit/sparklibs.json" "$srcDir/tests/cluster/mount/"
 
 dbx deploy --deployment-file  "$srcDir/tests/cluster/mount/setup_job.yml.j2"
 

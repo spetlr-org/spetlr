bc91f2f33d9bc631261b097f955d2963ac97b9c7
Lau Johansson <Lau_johansson@hotmail.com>
2022-08-05 14:07:03 +0200

    add tablenames to configurator

--- tests/cluster/sql/test_deliveryexecutor.py
+++ tests/cluster/sql/test_deliveryexecutor.py
@@ -1,15 +1,24 @@
 import unittest
 
+from atc.config_master import TableConfigurator
 from tests.cluster.sql.DeliverySqlExecutor import DeliverySqlExecutor
 from tests.cluster.sql.DeliverySqlServer import DeliverySqlServer
 
+from . import extras
+
 
 class DeliverySqlExecutorTests(unittest.TestCase):
+    tc = None
     sql_server = None
 
     @classmethod
     def setUpClass(cls):
         cls.sql_server = DeliverySqlServer()
+
+        # Register the delivery table for the table configurator
+        cls.tc = TableConfigurator()
+        cls.tc.add_resource_path(extras)
+        cls.tc.set_debug()
 
     @classmethod
     def tearDownClass(cls):

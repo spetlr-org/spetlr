f434a4ee36a1d1d1db5183fe93390a3c171b28ed
Lau Johansson <Lau_johansson@hotmail.com>
2022-08-24 15:37:17 +0200

    create drop oldest duplicate incl test

--- src/atc/transformers/__init__.py
+++ src/atc/transformers/__init__.py
@@ -1,2 +1,5 @@
+from .drop_oldest_duplicate_transformer import (  # noqa: F401
+    DropOldestDuplicatesTransformer,
+)
 from .simple_sql_transformer import SimpleSqlServerTransformer  # noqa: F401
 from .union_transformer import UnionTransformer  # noqa: F401
---
+++ src/atc/transformers/drop_oldest_duplicate_transformer.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from pyspark.sql import DataFrame
+
+from atc.etl import Transformer
+from atc.utils import DropOldestDuplicates
+
+
+class DropOldestDuplicatesTransformer(Transformer):
+    def __init__(self, *, cols: List[str], orderByColumn: str):
+        super().__init__()
+        self.cols = cols
+        self.orderByColumn = orderByColumn
+
+    def process(self, df: DataFrame) -> DataFrame:
+
+        return DropOldestDuplicates(
+            df=df, cols=self.cols, orderByColumn=self.orderByColumn
+        )

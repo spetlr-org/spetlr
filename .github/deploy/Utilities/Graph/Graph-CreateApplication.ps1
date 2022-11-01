4c5a5228dab431aaa14f264bc5f73c5d7d2f608f
Lau Johansson <Lau_johansson@hotmail.com>
2022-11-01 09:12:54 +0100

    feat: add identifieruri to create app

--- .github/deploy/Utilities/Graph/Graph-CreateApplication.ps1
+++ .github/deploy/Utilities/Graph/Graph-CreateApplication.ps1
@@ -1,11 +1,20 @@
 # Graph equivalent of az ad app create
 function Graph-CreateApplication {
 
-    param (
-      [Parameter(Mandatory=$true)]
-      [string]
-      $displayName
-    )
+  param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $displayName,
+    [Parameter(Mandatory = $false)]
+    [string]
+    $identifierUri
+  )
+  if ($null -eq $identifierUri) {
+    $app = Graph-Rest -method "post" -url "applications" -body @{displayName = $displayName }
+  }
+  else {
+    $app = Graph-Rest -method "post" -url "applications" -body @{displayName = $displayName; identifierUris = @($identifierUri)}
+  }
 
-    return Graph-Rest -method "post" -url "applications" -body @{displayName=$displayName}
+  return $app
 }

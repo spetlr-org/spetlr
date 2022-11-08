69a24dc4105342ffbb0cfccd9c2497407f4c98ae
Simon Heisterkamp <set>
2022-11-08 12:42:37 +0000

    rename steps and force install

--- .github/deploy/steps/20-Install-Ps-Requirements.ps1
+++ .github/deploy/steps/20-Install-Ps-Requirements.ps1
@@ -2,5 +2,5 @@ Write-Host "  Install powershell dependencies..." -ForegroundColor Yellow
 #
 # Install-Module -Name Az.Accounts -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
 Write-Host "  Install SqlServer (Invoke-SqlCmd)" -ForegroundColor DarkYellow
-Install-Module -Name SqlServer -Confirm
+Install-Module -Name SqlServer -Force
 

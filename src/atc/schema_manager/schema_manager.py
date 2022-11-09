82bbcad65c5746a56c349b4c77af55f85b35cdf1
Thomas <tbt@delegate.dk>
2022-11-09 15:00:14 +0100

    feat: add schema manager get_all_schemas

--- src/atc/schema_manager/schema_manager.py
+++ src/atc/schema_manager/schema_manager.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Dict
 
 import pyspark.sql.types as T
@@ -15,49 +16,92 @@ class FalseSchemaDefinitionException(AtcException):
     pass
 
 
+class UnregisteredSchemaDefinitionException(AtcException):
+    pass
+
+
 class SchemaManager(metaclass=Singleton):
 
     # This dict contains the registered schemas
-    registered_schemas: Dict[str, T.StructType]
+    _registered_schemas: Dict[str, T.StructType] = {}
 
     def __init__(self):
-        self.reset()
+        pass
 
-    def reset(self) -> None:
-        self.registered_schemas = {}
+    def register_schema(self, schema_name: str, schema: T.StructType):
+        self._registered_schemas[schema_name] = schema
 
-    def register_schema(self, schema_name: str, schema: T.StructType) -> T.StructType:
-        self.registered_schemas[schema_name] = schema
+    def get_schema(self, schema_identifier: str):
+        # Check if the identifier is a registered schema
+        if schema_identifier in self._registered_schemas.keys():
+            return self._registered_schemas[schema_identifier]
 
-    def get_registered_schema(self, schema_name: str) -> T.StructType:
-        if schema_name not in self.registered_schemas:
-            raise NoSuchSchemaException(property)
+        # Otherwise, check if the schema identifier is a table identifier
+        try:
+            schema = TableConfigurator().table_property(
+                table_id=schema_identifier, property_name="schema"
+            )
+        except ValueError:
+            raise NoSuchSchemaException(schema_identifier)
 
-        return self.registered_schemas[schema_name]
+        # If the schema is a string, check if it has been registered
+        if isinstance(schema, str) and schema in self._registered_schemas.keys():
+            return self._registered_schemas[schema]
 
-    def get_schema_as_string(self, table_id: str) -> str:
-        schema = TableConfigurator().table_property(
-            table_id=table_id, property_name="schema"
+        # Otherwise, it must be a dict
+        if not isinstance(schema, dict):
+            raise UnregisteredSchemaDefinitionException(schema)
+
+        dict_items = list(schema.items())
+        if len(dict_items) != 1:
+            raise FalseSchemaDefinitionException(schema)
+
+        schema_tuple = dict_items[0]
+        # Check if the schema is a sql string
+        if schema_tuple[0] == "sql":
+            return T._parse_datatype_string(schema_tuple[1])
+
+        # TODO: Add additional schema types here
+        else:
+            raise FalseSchemaDefinitionException(schema)
+
+    def _schema_to_string(self, schema: T.StructType):
+        # TODO: Create a more capable method of translating StructTypes to strings
+        # Lacking:
+        # - generated-always-as
+        # - comments
+
+        str_schema = ", ".join(
+            [f"{field.name} {field.dataType.simpleString()}" for field in schema.fields]
         )
 
-        # Check if the schema is a python reference
-        if isinstance(schema, str):
-            if schema in self.registered_schemas:
-                schema = self.registered_schemas[schema]
+        return str_schema
 
-        elif isinstance(schema, dict):
-            dict_items = list(schema.items())
+    def get_schema_as_string(self, schema_identifier: str):
+        schema = self.get_schema(schema_identifier)
 
-            if len(dict_items) != 1:
-                raise FalseSchemaDefinitionException(property)
+        str_schema = self._schema_to_string(schema)
 
-            dict_tuple = dict_items[0]
-            print(dict_tuple)
-            # Check if the schema is a sql string
-            if dict_tuple[0] == "sql":
-                print(dict_tuple[1])
-                schema = T._parse_datatype_string(dict_tuple[1])
-                print(schema)
-            # TODO: Add additionaly schema types here
+        return str_schema
 
-        return schema
+    def get_all_schemas(self):
+        out_schemas = copy.deepcopy(self._registered_schemas)
+
+        table_details = TableConfigurator().get_all_details()
+
+        for name, value in list(table_details.items()):
+            if "_schema" in name:
+                table_name = name.replace("_schema", "")
+                out_schemas[table_name] = value
+
+        return out_schemas
+
+    def get_all_spark_sql_schemas(self):
+        schemas_dict = self.get_all_schemas()
+        str_schemas = {}
+
+        for name, schema in list(schemas_dict.items()):
+            str_schema = self._schema_to_string(schema)
+            str_schemas[name] = str_schema
+
+        return str_schemas
--- tests/cluster/schema_manager/test_schema_manager.py
+++ tests/cluster/schema_manager/test_schema_manager.py
@@ -19,49 +19,104 @@ class TestSchemaManager(unittest.TestCase):
             ]
         )
 
-        self.assertNotIn("register_test_schema", manager.registered_schemas.keys())
+        self.assertNotIn("register_test", manager._registered_schemas.keys())
 
-        manager.register_schema(schema_name="register_test_schema", schema=schema)
+        manager.register_schema(schema_name="register_test", schema=schema)
 
-        self.assertIn("register_test_schema", manager.registered_schemas.keys())
+        self.assertIn("register_test", manager._registered_schemas.keys())
 
     def test_get_registered_schema(self):
-        tc = TableConfigurator()
-        tc.add_resource_path(extras)
+        TableConfigurator().add_resource_path(extras)
         manager = SchemaManager()
 
-        schema = extras.python_test_schema
-
-        self.assertEqual(
-            manager.get_registered_schema(schema_name="python_test_schema"), schema
+        schema = T.StructType(
+            [
+                T.StructField("Column1", T.IntegerType(), True),
+                T.StructField("Column2", T.StringType(), True),
+                T.StructField("Column3", T.FloatType(), True),
+            ]
         )
 
-    def test_get_python_schema(self):
-        str_schema = SchemaManager().get_schema_as_string(table_id="SchemaTestTable1")
+        self.assertNotIn("register_test2", manager._registered_schemas.keys())
 
-        self.assertEqual(str_schema, "a int, b string,")
+        manager.register_schema(schema_name="register_test2", schema=schema)
+
+        self.assertEqual(
+            manager.get_schema(schema_identifier="register_test2"),
+            schema,
+        )
+
+    def test_get_python_ref_schema(self):
+        TableConfigurator().add_resource_path(extras)
+
+        schema = SchemaManager().get_schema(schema_identifier="SchemaTestTable1")
+
+        expected_schema = extras.python_test_schema
+
+        self.assertEqual(schema, expected_schema)
 
     def test_get_sql_schema(self):
-        str_schema = SchemaManager().get_schema_as_string(table_id="SchemaTestTable2")
+        TableConfigurator().add_resource_path(extras)
 
-        self.assertEqual(str_schema, "a INTEGER, b STRING,")
+        schema = SchemaManager().get_schema(schema_identifier="SchemaTestTable2")
+
+        expected_schema = T.StructType(
+            [
+                T.StructField("a", T.IntegerType(), True),
+                T.StructField("b", T.StringType(), True),
+            ]
+        )
+
+        self.assertEqual(schema, expected_schema)
 
     # TODO #
-    def test_read_json_schema(self):
+    def test_get_json_schema(self):
         pass
 
     # TODO #
-    def test_read_python_schema(self):
+    def test_get_python_schema(self):
         pass
 
     def test_get_schema_as_string(self):
-        pass
+        TableConfigurator().add_resource_path(extras)
+
+        schema = SchemaManager().get_schema_as_string(
+            schema_identifier="SchemaTestTable2"
+        )
+
+        self.assertEqual(schema, "a INTEGER, b STRING,")
 
     def test_get_all_schemas(self):
-        pass
+        TableConfigurator().add_resource_path(extras)
+
+        schemas_dict = SchemaManager().get_all_schemas()
+
+        expected_schemas = {
+            "python_test_table": extras.python_test_schema,
+            "SchemaTestTable1": extras.python_test_schema,
+            "SchemaTestTable2": T.StructType(
+                [
+                    T.StructField("a", T.IntegerType(), True),
+                    T.StructField("b", T.StringType(), True),
+                ]
+            ),
+        }
+
+        self.assertDictEqual(schemas_dict, expected_schemas)
 
     def test_get_all_spark_sql_schemas(self):
-        pass
+        TableConfigurator().add_resource_path(extras)
+
+        schemas_dict = SchemaManager().get_all_spark_sql_schemas()
+
+        test_table_string = "a int, b int, c string, cplx struct<someId:string,details:struct<id:string>,blabla:array<int>>, d timestamp, m map<int,string>, p decimal(10,3), final string"
+        expected_schemas = {
+            "python_test_table": test_table_string,
+            "SchemaTestTable1": test_table_string,
+            "SchemaTestTable2": "a INTEGER, b STRING,",
+        }
+
+        self.assertDictEqual(schemas_dict, expected_schemas)
 
 
 if __name__ == "__main__":

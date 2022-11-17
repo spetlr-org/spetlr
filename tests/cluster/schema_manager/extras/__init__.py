ac46b4e777411e48b67fd30b7e52b4fd29a0d5ac
Thomas <tbt@delegate.dk>
2022-11-17 13:13:37 +0100

    feat: change register method

--- tests/cluster/schema_manager/extras/__init__.py
+++ tests/cluster/schema_manager/extras/__init__.py
@@ -1,7 +1,5 @@
-from atc.schema_manager import SchemaManager
+from atc.schema_manager.schema_manager import register_schema
 
 from .python_schemas import python_test_schema
 
-SchemaManager().register_schema(
-    schema_name="python_test_schema", schema=python_test_schema
-)
+register_schema(schema_name="python_test_schema", schema=python_test_schema)

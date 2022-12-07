0b77cf82861b3553db24459c2726f30f281014f7
Jeppe Blixenkrone-Møller <jel@delegate.dk>
2022-12-07 14:53:05 +0100

    fix: Error from missing pyclean module in build script

--- .github/submit/build.ps1
+++ .github/submit/build.ps1
@@ -4,7 +4,7 @@ $srcDir = "$PSScriptRoot/../.."
 Push-Location -Path $srcDir
 
 python -m pip install --upgrade pip
-pip install setuptools wheel
+pip install setuptools wheel pyclean
 
 # we want a really clean build (even locally)
 if(Test-Path -Path dist)

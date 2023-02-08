44a0b95d4886cc70c3e0f704e2f7b322f6c3ccf9
Lasse Agerlund Jensen <laj@delegate.dk>
2023-02-08 21:14:36 +0100

    add parameter for overlapping period

--- src/atc/etl/extractors/incremental_extractor.py
+++ src/atc/etl/extractors/incremental_extractor.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 import pyspark.sql.functions as f
 from pyspark.sql import DataFrame
 
@@ -9,7 +11,7 @@ from atc.etl.extractors.simple_extractor import Readable
 class IncrementalExtractor(Extractor):
     """This extractor will extract from any object that has a .read() method.
     Furthermore, it will use a target table for enabling incremental extraction.
-
+    If needed it's possible to define a overlapping period in days.
     NB: It is not recommended to use this on Eventhub data.
         Use EventHubCaptureExtractor instead.
 
@@ -21,8 +23,8 @@ class IncrementalExtractor(Extractor):
         handle_target: Readable,
         time_col_source: str,
         time_col_target: str,
-        overlap_days: str = None,
         dataset_key: str = None,
+        overlap_days: int = None,
     ):
         super().__init__(dataset_key=dataset_key)
         self.handle_source = handle_source
@@ -32,7 +34,6 @@ class IncrementalExtractor(Extractor):
         self._overlap_days = overlap_days
 
     def read(self) -> DataFrame:
-
         if isinstance(self.handle_source, EventHubCapture):
             print(
                 "It is recommended to use EventHubCaptureExtractor "
@@ -48,6 +49,10 @@ class IncrementalExtractor(Extractor):
         target_max_time = (
             df_target.groupBy().agg(f.max(self._timecol_target)).collect()[0][0]
         )
+
+        # If overlap_days is defined extract it from target_max_time
+        if self._overlap_days:
+            target_max_time = target_max_time - timedelta(days=self._overlap_days)
 
         # If the target table is empty, target_max_time will be None
         # Only filter the input dataframe if the table is non-empty

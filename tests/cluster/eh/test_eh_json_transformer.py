85b504660bc9cd09d5d19155857c277f52ff00e5
Lau Johansson <Lau_johansson@hotmail.com>
2023-02-28 09:31:59 +0100

    fix dataframe creation

--- tests/cluster/eh/test_eh_json_transformer.py
+++ tests/cluster/eh/test_eh_json_transformer.py
@@ -2,13 +2,7 @@ import json
 
 from atc_tools.testing import DataframeTestCase
 from atc_tools.time import dt_utc
-from pyspark.sql.types import (
-    BinaryType,
-    DateType,
-    StructField,
-    StructType,
-    TimestampType,
-)
+from pyspark.sql.types import BinaryType, StructField, StructType, TimestampType
 
 from atc import Configurator
 from atc.delta import DeltaHandle
@@ -23,7 +17,7 @@ class JsonEhTransformerUnitTests(DataframeTestCase):
     capture_eventhub_output_schema = StructType(
         [
             StructField("Body", BinaryType(), True),
-            StructField("pdate", DateType(), True),
+            StructField("pdate", TimestampType(), True),
             StructField("EnqueuedTimestamp", TimestampType(), True),
         ]
     )
@@ -36,16 +30,18 @@ class JsonEhTransformerUnitTests(DataframeTestCase):
 
         cls.tc.register("TblPdate1", {"name": "TablePdate1{ID}"})
         cls.tc.register("TblPdate2", {"name": "TablePdate2{ID}"})
+        cls.tc.register("TblPdate3", {"name": "TablePdate3{ID}"})
 
         spark = Spark.get()
 
         spark.sql(f"DROP TABLE IF EXISTS {cls.tc.table_name('TblPdate1')}")
         spark.sql(f"DROP TABLE IF EXISTS {cls.tc.table_name('TblPdate2')}")
+        spark.sql(f"DROP TABLE IF EXISTS {cls.tc.table_name('TblPdate3')}")
 
         spark.sql(
             f"""
             CREATE TABLE {cls.tc.table_name('TblPdate1')}
-            (id int, name string, BodyJson string, pdate timestamp)
+            (id int, name string, BodyJson string, pdate timestamp, EnqueuedTimestamp timestamp)
             PARTITIONED BY (pdate)
         """
         )
@@ -53,9 +49,17 @@ class JsonEhTransformerUnitTests(DataframeTestCase):
         spark.sql(
             f"""
                     CREATE TABLE {cls.tc.table_name('TblPdate2')}
-                    (id int, name string, pdate timestamp)
+                    (id int, name string, pdate timestamp, EnqueuedTimestamp timestamp)
                     PARTITIONED BY (pdate)
                 """
+        )
+
+        spark.sql(
+            f"""
+                            CREATE TABLE {cls.tc.table_name('TblPdate3')}
+                            (id int, name string, pdate timestamp, EnqueuedTimestamp timestamp, Unknown string)
+                            PARTITIONED BY (pdate)
+                        """
         )
 
     def test_transformer_w_body(self):
@@ -64,14 +68,16 @@ class JsonEhTransformerUnitTests(DataframeTestCase):
 
         df_in = Spark.get().createDataFrame(
             [
-                json.dumps(
-                    {
-                        "id": "1234",
-                        "name": "John",
-                    }
-                ).encode("utf-8"),
-                dt_utc(2021, 10, 31, 0, 0, 0).date,  # pdate
-                dt_utc(2021, 10, 31, 0, 0, 0),  # EnqueuedTimestamp
+                (
+                    json.dumps(
+                        {
+                            "id": "1234",
+                            "name": "John",
+                        }
+                    ).encode("utf-8"),
+                    dt_utc(2021, 10, 31, 0, 0, 0),  # pdate
+                    dt_utc(2021, 10, 31, 0, 0, 0),  # EnqueuedTimestamp
+                ),
             ],
             self.capture_eventhub_output_schema,
         )
@@ -87,7 +93,8 @@ class JsonEhTransformerUnitTests(DataframeTestCase):
                             "name": "John",
                         }
                     ),
-                    dt_utc(2021, 10, 31, 0, 0, 0).date,
+                    dt_utc(2021, 10, 31, 0, 0, 0),
+                    dt_utc(2021, 10, 31, 0, 0, 0),
                 ),
             ],
             dh.read().schema,
@@ -110,7 +117,7 @@ class JsonEhTransformerUnitTests(DataframeTestCase):
                         "name": "John",
                     }
                 ).encode("utf-8"),
-                dt_utc(2021, 10, 31, 0, 0, 0).date,  # pdate
+                dt_utc(2021, 10, 31, 0, 0, 0),  # pdate
                 dt_utc(2021, 10, 31, 0, 0, 0),  # EnqueuedTimestamp
             ],
             self.capture_eventhub_output_schema,
@@ -121,7 +128,8 @@ class JsonEhTransformerUnitTests(DataframeTestCase):
                 (
                     1234,
                     "John",
-                    dt_utc(2021, 10, 31, 0, 0, 0).date,
+                    dt_utc(2021, 10, 31, 0, 0, 0),
+                    dt_utc(2021, 10, 31, 0, 0, 0),
                 ),
             ],
             dh.read().schema,
@@ -135,4 +143,36 @@ class JsonEhTransformerUnitTests(DataframeTestCase):
     def test_transformer_unknown_target_field(self):
         """This should test what happens if the target
         schema has a field that does not exist in the source dataframe."""
-        pass
+        dh = DeltaHandle.from_tc("TblPdate3")
+
+        df_in = Spark.get().createDataFrame(
+            [
+                json.dumps(
+                    {
+                        "id": "1234",
+                        "name": "John",
+                    }
+                ).encode("utf-8"),
+                dt_utc(2021, 10, 31, 0, 0, 0),  # pdate
+                dt_utc(2021, 10, 31, 0, 0, 0),  # EnqueuedTimestamp
+            ],
+            self.capture_eventhub_output_schema,
+        )
+
+        expected = Spark.get().createDataFrame(
+            [
+                (
+                    1234,
+                    "John",
+                    dt_utc(2021, 10, 31, 0, 0, 0),
+                    dt_utc(2021, 10, 31, 0, 0, 0),
+                    None,
+                ),
+            ],
+            dh.read().schema,
+        )
+
+        df_result = EhJsonToDeltaTransformer(target_dh=dh).process(df_in)
+
+        # Check that data is correct
+        self.assertDataframeMatches(df_result, expected)

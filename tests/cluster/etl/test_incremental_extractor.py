f127cc31cc0fbdb0d90315c8fbf5a216da5f2957
Lasse Agerlund Jensen <laj@delegate.dk>
2023-04-14 13:32:02 +0200

    fix overlap period

--- tests/cluster/etl/test_incremental_extractor.py
+++ tests/cluster/etl/test_incremental_extractor.py
@@ -252,3 +252,47 @@ class IncrementalExtractorTests(DataframeTestCase):
         df_extract = extractor.read()
 
         self.assertDataframeMatches(df_extract, None, self.extractOverlap)
+
+    def test_05_can_extract_with_overlap_empty_target(self):
+        """
+        Source has the following data:
+        |id| stringcol    | timecol          |
+        |--|--------------|------------------|
+        |2 | "string2"    | 01.01.2021 10:55 |
+        |3|  "string3"    | 01.01.2021 11:00 |
+        |4 | "string4"    | 05.01.2021 11:00 |
+
+        Target has no data:
+
+        |id| stringcol    | timecol          |
+        |--|--------------|------------------|
+        |  |              |                  |
+
+        All data should been read:
+
+        |id| stringcol    | timecol          |
+        |--|--------------|------------------|
+        |2 | "string2"    | 01.01.2021 10:55 |
+        |3|  "string3"    | 01.01.2021 11:00 |
+        |4 | "string4"    | 05.01.2021 11:00 |
+        """
+        source_test_handle = TestHandle(
+            provides=DataframeCreator.make_partial(
+                self.dummy_schema, self.dummy_columns, self.sourceOverlap
+            )
+        )
+        target_test_handle = TestHandle(
+            provides=DataframeCreator.make_partial(
+                self.dummy_schema, self.dummy_columns, []
+            )
+        )
+        extractor = IncrementalExtractor(
+            handle_source=source_test_handle,
+            handle_target=target_test_handle,
+            time_col_source="timecol",
+            time_col_target="timecol",
+            dataset_key="source",
+            overlap_period=timedelta(days=5),
+        )
+        df_extract = extractor.read()
+        self.assertDataframeMatches(df_extract, None, self.extractOverlap)

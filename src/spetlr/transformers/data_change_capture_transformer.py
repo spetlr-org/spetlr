81e7064b31c2f923e658e56a4a2deed11d489775
Lasse Agerlund Jensen <laj@delegate.dk>
2023-06-05 14:46:07 +0200

    finalize tests

--- src/spetlr/transformers/data_change_capture_transformer.py
+++ src/spetlr/transformers/data_change_capture_transformer.py
@@ -1,18 +1,19 @@
 import warnings
-
+from typing import List
 from pyspark.sql.functions import md5, concat_ws, col
+from pyspark.sql import DataFrame
 from spetlr.etl.transformer import Transformer
+from spetlr.etl.types import dataset_group
 
 
-
-
-class CaptureHistoryTransformer(Transformer):
+class DataChangeCaptureTransformer(Transformer):
     """
-       This class archives data changes from a source table, using a primary key.
-       It identifies and captures new or modified records.
-       For accurate comparison of all columns, ensure both schemas are identical.
+    This class archives data changes from a source table, using a primary key column.
+    It identifies and captures new or modified records.
+    For accurate comparison of all columns, ensure both schemas are identical.
 
     """
+
     def __init__(self, primary_key, cols_to_exclude: List[str] = None):
         self.primary_key = primary_key
         if cols_to_exclude is None:
@@ -22,32 +23,33 @@ class CaptureHistoryTransformer(Transformer):
         else:
             self.cols_to_exclude = cols_to_exclude
 
-
     def _md5_hash(self, df: DataFrame, colName: str):
         cols = [col for col in df.columns if col not in self.cols_to_exclude]
         return df.withColumn(colName, md5(concat_ws("||", *cols)))
 
-    def _warn_if_schema_differs(df1: DataFrame, df2: DataFrame):
+    def _warn_if_schema_differs(self, df1: DataFrame, df2: DataFrame):
         if df1.schema != df2.schema:
             warnings.warn("Schemas are not identical")
 
-    def process_many(self, datasets: dataset_group) -> DataFrame:
-
+    def process_many(self, dataset: dataset_group) -> DataFrame:
         df_source = dataset["source"]
         df_target = dataset["target"]
 
         self._warn_if_schema_differs(df_source, df_target)
 
         # Add an md5 hash column to both DataFrames
-        df_source = self._md5_hash(df_source, 'md5_source')
-        df_target = self._md5_hash(df_target, 'md5_target')
-        df_joined = df_source.join(df_target, self.primary_key, 'left_outer')
+        df_source = self._md5_hash(df_source, "md5_source")
+        df_target = self._md5_hash(df_target, "md5_target")
+        df_joined_2 = df_source.join(df_target, self.primary_key, "left_outer")
+        df_joined = df_source.join(
+            df_target.select("md5_target", self.primary_key),
+            self.primary_key,
+            "left_outer",
+        )
 
         # Filter out rows where the MD5 hash values are different or the target values are null
-        df = df_joined.filter((df_joined.md5_source != df_joined.md5_target) | df_joined.md5_target.isNull())
+        df = df_joined.filter(
+            (df_joined.md5_source != df_joined.md5_target)
+            | df_joined.md5_target.isNull()
+        )
         return df
-
-
-
-
-

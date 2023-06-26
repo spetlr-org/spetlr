167b8825fdfd4831b730f85b2b5cad7a7029f788
Lau Johansson <ljo@delegate.dk>
2023-06-26 13:22:29 +0200

    init all extractors

--- src/spetlr/etl/extractors/__init__.py
+++ src/spetlr/etl/extractors/__init__.py
@@ -1,3 +1,9 @@
-from .incremental_extractor import IncrementalExtractor  # noqa: F401
-from .simple_extractor import SimpleExtractor  # noqa: F401
-from .stream_extractor import StreamExtractor  # noqa: F401
+from .incremental_extractor import IncrementalExtractor
+from .simple_extractor import SimpleExtractor
+from .stream_extractor import StreamExtractor
+
+__all__ = [
+    "IncrementalExtractor",
+    "SimpleExtractor",
+    "StreamExtractor",
+]

dd88661740f3efb145c127bfd073ef4dbb423a95
Lau Johansson <ljo@delegate.dk>
2023-06-27 10:12:07 +0200

    use repr e in fileexists

--- src/spetlr/dbutils/FileExists.py
+++ src/spetlr/dbutils/FileExists.py
@@ -4,26 +4,13 @@ from spetlr.functions import init_dbutils
 def file_exists(path: str) -> bool:
     """
     Helper function to check whether a file or folder exists.
-
-    Todo: Feel free to implement a better solution like the following:
-
-    This implementation should be more correct:
-
-    try:
-        init_dbutils().fs.ls(path)
-        return True
-    except Exception as e:
-        exc_info = sys.exc_info()
-        if "java.io.FileNotFoundException" in str(exc_info):
-            return False
-        else:
-            raise e
-
-
     """
 
     try:
         init_dbutils().fs.ls(path)
         return True
-    except Exception:
-        return False
+    except Exception as e:
+        if "java.io.FileNotFoundException" in repr(e):
+            return False
+        else:
+            raise e

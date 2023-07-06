d096528181f9849dff9ad6ff5736bc07babbf06f
Simon Heisterkamp <simon@heisterkamp.dk>
2023-07-06 15:27:00 +0200

    restore db-auto to working version

--- src/spetlr/db_auto.py
+++ src/spetlr/db_auto.py
@@ -1,16 +1,45 @@
 """Simplified access to the databricks api."""
 
+import json
+import sys
+
 from databricks.sdk import WorkspaceClient
 
-import spetlr
+from spetlr.functions import init_dbutils
 
 
 def getDbApi() -> WorkspaceClient:
     """
-    This method automatically configures a databricks API client.
-    In local running, the databricks-cli is used for configuration.
+    In local running, the databricks-sdk is used for configuration.
     Running on a cluster, the configuration is extracted from the job context.
     """
-    # Since the introduction of databricks-sdk this function is basically obsolete
-    # This also closes #60
-    return WorkspaceClient(product="spetlr", product_version=spetlr.__version__)
+    try:
+        dbutils = init_dbutils()
+    except ModuleNotFoundError:
+        # probably we are not in notebook
+        dbutils = None
+
+    if dbutils:
+        context = json.loads(
+            dbutils.notebook.entry_point.getDbutils().notebook().getContext().toJson()
+        )
+        host = context["extraContext"]["api_url"]
+        token = context["extraContext"]["api_token"]
+    else:
+        try:
+            cfg = WorkspaceClient().api_client
+
+        except ModuleNotFoundError:
+            print(
+                "In local running, databricks-cli needs to be installed.",
+                file=sys.stderr,
+            )
+            raise
+
+        host = cfg.host
+        token = cfg.token
+
+    if not host or not token:
+        raise Exception("Unable to auto-configure api client.")
+
+    return WorkspaceClient(host=host, token=token)

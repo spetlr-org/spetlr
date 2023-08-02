412b25974f25060f104a9b46795354e06ca4012b
Martin Bøge <>
2023-08-02 19:24:44 +0200

    wip feat: file handle read method

--- src/spetlr/filehandle/file_handle.py
+++ src/spetlr/filehandle/file_handle.py
@@ -1,4 +1,7 @@
+from typing import Any, Dict
+
 from pyspark.sql import DataFrame
+from pyspark.sql.types import StructType
 
 from spetlr.configurator.configurator import Configurator
 from spetlr.delta.delta_handle import DeltaHandleInvalidFormat
@@ -12,20 +15,38 @@ class FileHandle(TableHandle):
         self,
         *,
         file_location: str,
-        schema_location: str,
         data_format: str,
-        options: dict = None
+        schema: StructType = None,
+        schema_location: str = None,
+        options: Dict[str, Any] = None,
     ):
         """
-        file_location: the path to the file location of the delta table
-        schema_location: The location of the cloudfile schema.
-            Databricks documentation uses the expected writeStream checkpoint_path
-            as schema_location, see documentation at
-            https://docs.databricks.com/getting-started/etl-quick-start.html#auto-loader
+        A handle for reading files.
 
-        data_format: The expected data format extracted by the Autoloader.
-            Examples: JSON, CSV, PARQUET, AVRO, ORC, TEXT, and BINARYFILE.
-            See: https://docs.databricks.com/ingestion/auto-loader/index.html
+        Arguments:
+            file_location (str): The path where the files are stored. When using
+                'read_stream'. see documentation at:
+                https://docs.databricks.com/ingestion/auto-loader/index.html
+            data_format (str): The expected data format to be handled. Examples: JSON,
+                CSV, PARQUET, AVRO, ORC, TEXT, and BINARYFILE. When using 'read_stream'
+                Auto Loader processing is used, see documentation at:
+                https://docs.databricks.com/getting-started/etl-quick-start.html#auto-loader
+            schema (StructType, optional): The schema to apply when reading. If not
+                supplied the schema is inferred.
+            schema_location (str, optional): When 'read_stream' is used, the schema
+                location for storing the read schema. See Auto Loader documentation
+                provided above. It is recommended to use the checkpoint path.
+
+
+        Methods:
+            read() -> DataFrame: Reads the file(s) at the file location using the data
+                format provided.
+            read_stream() -> DataFrame: Reads the file(s) using Auto Loader processing.
+                See the documentation at:
+                https://docs.databricks.com/en/ingestion/auto-loader/index.html
+
+        Note that although the handle does not currently support writing in may be
+        implemented in the future.
         """
 
         if Spark.version() < Spark.DATABRICKS_RUNTIME_10_4:
@@ -36,6 +57,7 @@ class FileHandle(TableHandle):
 
         self._schema_location = schema_location
         self._options = options
+        self._schema = schema
 
         self._validate()
 
@@ -70,3 +92,16 @@ class FileHandle(TableHandle):
         reader = reader.load(self._location)
 
         return reader
+
+    def read(self) -> DataFrame:
+        reader = Spark.get().read
+
+        reader = reader.format(self._data_format)
+
+        if self._schema:
+            reader = reader.schema(self._schema)
+
+        if self._options:
+            reader = reader.options(**self._options)
+
+        return reader.load(self._location)

59b66ea34108581d970d2c098758bc00a69ae579
Lau Johansson <ljo@delegate.dk>
2023-08-11 12:51:49 +0200

    feat: add product and version to databricks sdk

--- src/spetlr/db_auto.py
+++ src/spetlr/db_auto.py
@@ -5,6 +5,8 @@ import sys
 
 from databricks.sdk import WorkspaceClient
 
+import spetlr.version as spetlrversion
+from spetlr import __name__ as spetlrname
 from spetlr.functions import init_dbutils
 
 
@@ -43,4 +45,6 @@ def getDbApi() -> WorkspaceClient:
     if not host or not token:
         raise Exception("Unable to auto-configure api client.")
 
-    return WorkspaceClient(host=host, token=token)
+    return WorkspaceClient(
+        host=host, token=token, product=spetlrname, product_version=spetlrversion
+    )

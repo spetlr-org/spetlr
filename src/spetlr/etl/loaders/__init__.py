6a3948514f183d64e3dd12ae01c4ffc9b97d5341
Simon Heisterkamp <simon@heisterkamp.dk>
2023-10-18 14:46:22 +0000

    delete loader to remove specific rows from a target table

--- src/spetlr/etl/loaders/__init__.py
+++ src/spetlr/etl/loaders/__init__.py
@@ -1,3 +1,4 @@
+from .delete_loader import DeleteLoader  # noqa: F401
 from .load_modes import Appendable, Overwritable, Upsertable  # noqa: F401
 from .simple_loader import SimpleLoader  # noqa: F401
 from .simple_sql_loader import SimpleSqlServerLoader  # noqa: F401

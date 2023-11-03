8a5e1ccfdab79eb4625616b941c20f3db7f29f40
Simon Heisterkamp <simon@heisterkamp.dk>
2023-11-03 09:26:59 +0000

    more test comments

--- tests/local/etl/test_lazy_extractor.py
+++ tests/local/etl/test_lazy_extractor.py
@@ -8,12 +8,16 @@ from spetlr.etl.extractors import LazySimpleExtractor
 
 class TestLazyExtractor(unittest.TestCase):
     def test_lazy_extractor(self):
-        handle = TestHandle(provides="bacon")
+        # This is the orchestrator. It takes any lunch it is given
+        # and returns it. If none is given, it extracts bacon and
+        # returns it.
         o = Orchestrator()
-        o.extract_from(LazySimpleExtractor(handle, "lunch"))
+        o.extract_from(LazySimpleExtractor(TestHandle(provides="bacon"), "lunch"))
 
+        # Given spam? return spam
         lazy_result = o.execute({"lunch": "spam"})
         self.assertEqual(lazy_result["lunch"], "spam")
 
+        # no lunch? use bacon
         active_result = o.execute()
         self.assertEqual(active_result["lunch"], "bacon")

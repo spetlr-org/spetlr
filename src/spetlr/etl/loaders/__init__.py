67a073b25c8c0e1743ec1b45d00de7576239f9a7
Martin Bøge Jørgensen <t3x-martin.joergensen@external.issworld.com>
2023-11-13 09:05:43 +0100

    feat: add init import for stream loader

--- src/spetlr/etl/loaders/__init__.py
+++ src/spetlr/etl/loaders/__init__.py
@@ -1,5 +1,6 @@
 from .load_modes import Appendable, Overwritable, Upsertable  # noqa: F401
 from .simple_loader import SimpleLoader  # noqa: F401
 from .simple_sql_loader import SimpleSqlServerLoader  # noqa: F401
+from .stream_loader import StreamLoader  # noqa: F401
 from .upsert_loader_streaming import UpsertLoaderStreaming  # noqa: F401
 from .UpsertLoader import UpsertLoader  # noqa: F401

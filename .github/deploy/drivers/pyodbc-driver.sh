f2730474d5ef9acf6dfade58da15a1c7ad414538
Jeppe Blixenkrone-Møller <jel@delegate.dk>
2024-01-22 11:49:13 +0100

    feat: Update odbc driver to use either ubuntu 20.2 or 22.2

--- .github/deploy/drivers/pyodbc-driver.sh
+++ .github/deploy/drivers/pyodbc-driver.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-sudo ACCEPT_EULA=Y apt-get install -q -y /dbfs/databricks/drivers/msodbcsql18_amd64.deb
+echo "Install package..."
+sudo apt-get update && sudo apt-get install -y lsb-release
+
+echo "Checking Ubuntu version..."
+ 
+# Get the release number
+release=$(lsb_release -r | awk '{print $2}')
+
+# Check if the release number matches 20.04 or 22.04 and execute the corresponding command
+if [ "$release" = "20.04" ]; then
+    echo "Detected Ubuntu 20.04. Installing package for 20.04..."
+    sudo ACCEPT_EULA=Y apt-get install -q -y /dbfs/databricks/drivers/msodbcsql18_amd64_ubuntu_20_04.deb
+elif [ "$release" = "22.04" ]; then
+    echo "Detected Ubuntu 22.04. Installing package for 22.04..."
+    sudo ACCEPT_EULA=Y apt-get install -q -y /dbfs/databricks/drivers/msodbcsql18_amd64_ubuntu_22_04.deb
+else
+    echo "This script is intended for Ubuntu 20.04 or 22.04. Your version is $release."
+fi
--- .github/deploy/steps/55-Pyodbc-Driver.ps1
+++ .github/deploy/steps/55-Pyodbc-Driver.ps1
@@ -1,16 +1,29 @@
 Write-Host "Setting up pyodbc driver" -ForegroundColor DarkYellow
 dbfs mkdirs dbfs:/databricks/drivers
 
-$odbcSource = "https://packages.microsoft.com/ubuntu/20.04/prod/pool/main/m/msodbcsql18/msodbcsql18_18.3.2.1-1_amd64.deb"
-$odbcSaveLocation = "$PSScriptRoot/../drivers/msodbcsql18_18.3.2.1-1_amd64.deb"
+$odbcSourceUbuntu204 = "https://packages.microsoft.com/ubuntu/20.04/prod/pool/main/m/msodbcsql18/msodbcsql18_18.3.2.1-1_amd64.deb"
+$odbcSaveLocationUbuntu204 = "$PSScriptRoot/../drivers/msodbcsql18_18.3.2.1-1_amd64_ubuntu_20_04.deb"
+$odbcDbSaveLocationUbuntu204 = "dbfs:/databricks/drivers/msodbcsql18_amd64_ubuntu_20_04.deb"
 
-Invoke-WebRequest -Uri $odbcSource -OutFile $odbcSaveLocation
+Invoke-WebRequest -Uri $odbcSourceUbuntu204 -OutFile $odbcSaveLocationUbuntu204
 
-if (-Not (Test-Path $odbcSaveLocation)) {
-  throw "msodbcsql18 was not succesfully downloaded."
+if (-Not (Test-Path $odbcSaveLocationUbuntu204)) {
+  throw "msodbcsql18 for ubuntu 20.04 was not succesfully downloaded."
 }
 
-dbfs cp --overwrite $odbcSaveLocation dbfs:/databricks/drivers/msodbcsql18_amd64.deb
+dbfs cp --overwrite $odbcSaveLocationUbuntu204 $odbcDbSaveLocationUbuntu204
+
+$odbcSourceUbuntu224 = "https://packages.microsoft.com/ubuntu/22.04/prod/pool/main/m/msodbcsql18/msodbcsql18_18.3.2.1-1_amd64.deb"
+$odbcSaveLocationUbuntu224 = "$PSScriptRoot/../drivers/msodbcsql18_18.3.2.1-1_amd64_ubuntu_22_04.deb"
+$odbcDbSaveLocationUbuntu224 = "dbfs:/databricks/drivers/msodbcsql18_amd64_ubuntu_22_04.deb"
+
+Invoke-WebRequest -Uri $odbcSourceUbuntu224 -OutFile $odbcSaveLocationUbuntu224
+
+if (-Not (Test-Path $odbcSaveLocationUbuntu224)) {
+  throw "msodbcsql18 for ubuntu 22.04 was not succesfully downloaded."
+}
+
+dbfs cp --overwrite $odbcSaveLocationUbuntu224 $odbcDbSaveLocationUbuntu224
 
 Set-DatabricksGlobalInitScript `
   -workspaceUrl $workspaceUrl `

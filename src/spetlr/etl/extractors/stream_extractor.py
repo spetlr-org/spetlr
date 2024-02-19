3fafaa66246202c9763ab6802d6723d05f124639
Jeppe Blixenkrone-Møller <jel@delegate.dk>
2024-02-19 17:06:29 +0100

    fix: Ensure dataset_key is None by default on all extractores

--- src/spetlr/etl/extractors/stream_extractor.py
+++ src/spetlr/etl/extractors/stream_extractor.py
@@ -7,7 +7,7 @@ from spetlr.tables import TableHandle
 class StreamExtractor(Extractor):
     """This extractor will extract from any object that has a .read_stream() method."""
 
-    def __init__(self, handle: TableHandle, dataset_key: str):
+    def __init__(self, handle: TableHandle, dataset_key: str = None):
         super().__init__(dataset_key=dataset_key)
         self.handle = handle
 

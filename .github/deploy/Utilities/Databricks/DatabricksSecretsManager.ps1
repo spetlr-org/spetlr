41b996b79903a23eb47a71922f5bc2102cedcb69
FarBo <farzadb58@gmail.com>
2024-08-28 11:53:20 +0200

    adopt new cli for databricks secrets

--- .github/deploy/Utilities/Databricks/DatabricksSecretsManager.ps1
+++ .github/deploy/Utilities/Databricks/DatabricksSecretsManager.ps1
@@ -2,25 +2,29 @@ class DatabricksSecretsManager {
   $secrets = @{}
 
 
-  [void] addSecret([string]$name, [string]$value){
+  [void] addSecret([string]$name, [string]$value) {
     # save for later so we can add it to databricks
     $this.secrets[$name] = @{
-      value=$value
+      value = $value
     }
   }
 
-  [void] pushToDatabricks([string]$db_secrets_scope){
+  [void] pushToDatabricks([string]$db_secrets_scope) {
     New-DatabricksScope -name $db_secrets_scope
 
 
     foreach ($name in $this.secrets.keys) {
 
-      databricks secrets put --scope $db_secrets_scope --key $name --string-value $this.secrets[$name].value
+      databricks secrets put --json "{
+        "scope": $db_secrets_scope,
+        "key": $name,
+        "string_value": $this.secrets[$name].value   
+      }"
       Write-Host "  Added secret '$($name)'"
     }
   }
 
-  [void] list(){
+  [void] list() {
     foreach ($name in $this.secrets.keys) {
       Write-Host $name
     }

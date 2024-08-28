0f383be6ae41993d608cd6fa36bda0a9f1bd3c05
FarBo <farzadb58@gmail.com>
2024-08-28 16:54:50 +0200

    try spark 13.3

--- .github/deploy/drivers/pyodbc-driver.sh
+++ .github/deploy/drivers/pyodbc-driver.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo ACCEPT_EULA=Y apt-get install -q -y /dbfs/databricks/drivers/msodbcsql17_amd64.deb
+# sudo ACCEPT_EULA=Y apt-get install -q -y /dbfs/databricks/drivers/msodbcsql18_amd64.deb

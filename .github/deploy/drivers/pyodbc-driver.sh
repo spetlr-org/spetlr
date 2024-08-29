0b18d78c203b42b0e62881658fc206bfd97cc32c
FarBo <farzadb58@gmail.com>
2024-08-29 11:44:39 +0200

    test disabling pyodbc insall

--- .github/deploy/drivers/pyodbc-driver.sh
+++ .github/deploy/drivers/pyodbc-driver.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 sudo ACCEPT_EULA=Y apt-get install -q -y /dbfs/databricks/drivers/msodbcsql17_amd64.deb
-# sudo ACCEPT_EULA=Y apt-get install -q -y /dbfs/databricks/drivers/msodbcsql18_amd64.deb
--- .github/deploy/steps/55-Pyodbc-Driver.ps1
+++ .github/deploy/steps/55-Pyodbc-Driver.ps1
@@ -1,19 +1,19 @@
-Write-Host "Setting up pyodbc driver" -ForegroundColor DarkYellow
-databricks fs mkdir dbfs:/databricks/drivers
+# Write-Host "Setting up pyodbc driver" -ForegroundColor DarkYellow
+# databricks fs mkdir dbfs:/databricks/drivers
 
-$odbcSource = "https://packages.microsoft.com/ubuntu/20.04/prod/pool/main/m/msodbcsql17/msodbcsql17_17.7.2.1-1_amd64.deb"
-$odbcSaveLocation = "$PSScriptRoot/../drivers/msodbcsql17_17.7.2.1-1_amd64.deb"
+# $odbcSource = "https://packages.microsoft.com/ubuntu/20.04/prod/pool/main/m/msodbcsql17/msodbcsql17_17.7.2.1-1_amd64.deb"
+# $odbcSaveLocation = "$PSScriptRoot/../drivers/msodbcsql17_17.7.2.1-1_amd64.deb"
 
-Invoke-WebRequest -Uri $odbcSource -OutFile $odbcSaveLocation
+# Invoke-WebRequest -Uri $odbcSource -OutFile $odbcSaveLocation
 
-if (-Not (Test-Path $odbcSaveLocation)) {
-  throw "msodbcsql17 was not succesfully downloaded."
-}
+# if (-Not (Test-Path $odbcSaveLocation)) {
+#   throw "msodbcsql17 was not succesfully downloaded."
+# }
 
-databricks fs cp --overwrite $odbcSaveLocation dbfs:/databricks/drivers/msodbcsql17_amd64.deb
+# databricks fs cp --overwrite $odbcSaveLocation dbfs:/databricks/drivers/msodbcsql17_amd64.deb
 
-Set-DatabricksGlobalInitScript `
-  -workspaceUrl $workspaceUrl `
-  -bearerToken $accessToken `
-  -initScriptName "pyodbc-driver" `
-  -initScriptContent (Get-Content "$PSScriptRoot/../drivers/pyodbc-driver.sh" -Raw)
+# Set-DatabricksGlobalInitScript `
+#   -workspaceUrl $workspaceUrl `
+#   -bearerToken $accessToken `
+#   -initScriptName "pyodbc-driver" `
+#   -initScriptContent (Get-Content "$PSScriptRoot/../drivers/pyodbc-driver.sh" -Raw)

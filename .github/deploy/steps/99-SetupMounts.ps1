c695e148b5a721fbc1edcab0ad5527fce300ae5d
FarBo <farzadb58@gmail.com>
2024-09-04 15:36:36 +0200

    mount dbfs logic change

--- .github/deploy/steps/99-SetupMounts.ps1
+++ .github/deploy/steps/99-SetupMounts.ps1
@@ -1,26 +1,26 @@
 
-# $srcDir = "$PSScriptRoot/../../.."
+$repoRoot = (git rev-parse --show-toplevel)
 
-# Push-Location -Path $srcDir
+$srcDir = "$repoRoot/tests/mount"
 
-# dbx configure
-# #copy "$srcDir/.github/submit/sparklibs.json" "$srcDir/tests/cluster/mount/"
+Push-Location -Path $srcDir
 
-# $mountsJson = (,@(
-#   @{
-#     storageAccountName=$resourceName
-#     secretScope="secrets"
-#     clientIdName="Databricks--ClientId"
-#     clientSecretName="Databricks--ClientSecret"
-#     tenantIdName="Databricks--TenantId"
-#     containers = [array]$($dataLakeContainers | ForEach-Object{ $_.name })
-#   }
-# ))
+Write-Host "Push Setup Mounts notebook to the workspace" -ForegroundColor DarkYellow
+databricks workspace mkdirs /Workspace/Shared/Mount
+databricks workspace import-dir --overwrite .  /Workspace/Shared/Mount
 
-# $mountsJson | ConvertTo-Json -Depth 4 | Set-Content "$srcDir/tests/cluster/mount/mounts.json"
+Write-Host "Create Setup Mounts job" -ForegroundColor DarkYellow
+$jobResponse = databricks jobs create --json '@job_config.json'
+$jobId = ($jobResponse | ConvertFrom-Json).job_id
+Write-Host "Job created with job_id: $jobId"
 
-# dbx deploy --deployment-file  "$srcDir/tests/cluster/mount/setup_job.yml.j2"
+Write-Host "Run Setup Mounts job and wait for the completion..." -ForegroundColor DarkYellow
+$runResponse = databricks jobs run-now $jobId
+$runId = ($runResponse | ConvertFrom-Json).run_id
+$runStatus = ($runResponse | ConvertFrom-Json).state.result_state
+Write-Host "Job run completed with id: $runId and status: $runStatus"
 
-# dbx launch --job="Setup Mounts" --trace --kill-on-sigterm
-
-# Pop-Location
+if ($runStatus -ne "SUCCESS") {
+    Write-Host "Job run failed with id: $runId and status: $runStatus"
+    exit 1
+}
---
+++ tests/mount/main.py
@@ -0,0 +1,31 @@
+# Databricks notebook source
+client_id = dbutils.secrets.get(scope="secrets", key="Databricks--ClientId")
+client_secret = dbutils.secrets.get(scope="secrets", key="Databricks--ClientSecret")
+tenant_id = dbutils.secrets.get(scope="secrets", key="Databricks--TenantId")
+resource_name = dbutils.secrets.get(scope="values", key="resourceName")
+
+configs = {
+    "fs.azure.account.auth.type": "OAuth",
+    "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+    "fs.azure.account.oauth2.client.id": client_id,
+    "fs.azure.account.oauth2.client.secret": client_secret,
+    "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token",
+}
+
+source = f"abfss://silver@{resource_name}.dfs.core.windows.net/"
+mount_point = f"/mnt/{resource_name}/silver/"
+
+# COMMAND ----------
+
+for mount in dbutils.fs.mounts():
+    if mount.mountPoint == mount_point:
+        dbutils.fs.unmount(mount_point)
+    break
+
+# COMMAND ----------
+
+dbutils.fs.mount(source=source, mount_point=mount_point, extra_configs=configs)
+
+# COMMAND ----------
+
+dbutils.fs.refreshMounts()

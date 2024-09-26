41ed65380fd00d3c0ee9095df80b50f9700b6bca
FarBo <farzadb58@gmail.com>
2024-09-26 07:58:23 +0200

    Test submit tests

--- .github/submit/connect_DB_api.ps1
+++ .github/submit/connect_DB_api.ps1
@@ -1,7 +1,18 @@
-$repoRoot = (git rev-parse --show-toplevel)
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $uniqueRunId
+)
 
-. "$repoRoot/.github/submit/config.ps1"
+###############################################################################################
+# Get the resource group name and resource name
+###############################################################################################
+$resourceGroupName  = "githubspetlr${$uniqueRunId}"  # This name is also used in the Terraform section
+$resourceName = "githubspetlr${$uniqueRunId}"  # This name is also used in the Terraform section
 
+Write-Host "Resource group name is $resourceGroupName"
+Write-Host "Resource name is $resourceName"
 ###############################################################################################
 # Connect to Databricks
 ###############################################################################################

718cd2bb85a52d12a6d6152a99a8a2da78296a85
FarBo <farzadb58@gmail.com>
2024-09-27 11:31:18 +0200

    object_id

--- .github/terraform/integration_resources/spn.tf
+++ .github/terraform/integration_resources/spn.tf
@@ -20,7 +20,7 @@ resource "azuread_service_principal_password" "captain" {
 resource "azurerm_role_assignment" "captain" {
   scope                = data.azurerm_subscription.primary.id
   role_definition_name = "Contributor"
-  principal_id         = azuread_service_principal.captain.id
+  principal_id         = azuread_service_principal.captain.object_id
 }
 
 # Set the cicd spn storage account role needed for eventhub capture blob  -------

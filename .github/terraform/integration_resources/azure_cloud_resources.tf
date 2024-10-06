0d127aeccfc8d9e0a020d0baf9ff0c9f0fc88e5a
Simon Heisterkamp <she@delegate.dk>
2024-10-06 23:19:17 +0000

    skip sql server for now

--- .github/terraform/integration_resources/azure_cloud_resources.tf
+++ .github/terraform/integration_resources/azure_cloud_resources.tf
@@ -138,29 +138,29 @@ resource "azurerm_eventhub_authorization_rule" "root" {
   manage              = true
 }
 
-# Provision sql ----------------------------------------------------------
-resource "azurerm_mssql_server" "sql" {
-  name                         = module.config.integration.resource_name
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = module.config.location
-  version                      = "12.0"
-  administrator_login          = "missadministrator"
-  administrator_login_password = "thisIsKat11"
-  minimum_tls_version          = "1.2"
-
-  azuread_administrator {
-    azuread_authentication_only = true
-    login_username              = module.config.integration.captain.display_name
-    object_id                   = azuread_service_principal.captain.object_id
-  }
-
-  # TODO: maybe missing firewall rules
-}
-
-resource "azurerm_mssql_database" "sql" {
-  name      = "spetlr"
-  server_id = azurerm_mssql_server.sql.id
-}
+# # Provision sql ----------------------------------------------------------
+# resource "azurerm_mssql_server" "sql" {
+#   name                         = module.config.integration.resource_name
+#   resource_group_name          = azurerm_resource_group.rg.name
+#   location                     = module.config.location
+#   version                      = "12.0"
+#   administrator_login          = "missadministrator"
+#   administrator_login_password = "thisIsKat11"
+#   minimum_tls_version          = "1.2"
+#
+#   azuread_administrator {
+#     azuread_authentication_only = true
+#     login_username              = module.config.integration.captain.display_name
+#     object_id                   = azuread_service_principal.captain.object_id
+#   }
+#
+#   # TODO: maybe missing firewall rules
+# }
+#
+# resource "azurerm_mssql_database" "sql" {
+#   name      = "spetlr"
+#   server_id = azurerm_mssql_server.sql.id
+# }
 
 # Provision access connector and setting its role ------------------------------
 resource "azurerm_databricks_access_connector" "ext_access_connector" {

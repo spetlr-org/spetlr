31b7e9c16aef70fd17919137864c124b3c7f878b
Simon Heisterkamp <she@delegate.dk>
2024-10-06 23:38:44 +0000

    defer secret scope till later

--- .github/terraform/integration_databricks/secret_scope.tf
+++ .github/terraform/integration_databricks/secret_scope.tf
@@ -1,17 +1,17 @@
 ## This module is create secret scope and secrets for the Databricks workspace ##
 
 # Secret scope for keyvault backed secrets -------------------------------------
-resource "databricks_secret_scope" "keyvault_backed_scope" {
-  provider = databricks.workspace
-
-  name = "secrets"
-
-  keyvault_metadata {
-    resource_id = data.azurerm_key_vault.key_vault.id
-    dns_name    = data.azurerm_key_vault.key_vault.vault_uri
-  }
-  depends_on = [databricks_service_principal_role.captain]
-}
+# resource "databricks_secret_scope" "keyvault_backed_scope" {
+#   provider = databricks.workspace
+#
+#   name = "secrets"
+#
+#   keyvault_metadata {
+#     resource_id = data.azurerm_key_vault.key_vault.id
+#     dns_name    = data.azurerm_key_vault.key_vault.vault_uri
+#   }
+#   depends_on = [databricks_service_principal_role.captain]
+# }
 
 # Secret scope and secrets for project genrated values -------------------------
 resource "databricks_secret_scope" "values" {

cdfa02bed2b705b4a93bc9ffbccaa072580066a4
Simon Heisterkamp <she@delegate.dk>
2024-10-19 23:58:47 +0000

    use shorter name due to azure restriction

--- .github/terraform/modules/config/main.tf
+++ .github/terraform/modules/config/main.tf
@@ -8,7 +8,7 @@ output "location" {
 }
 
 variable "uniqueRunId" {
-  type = string
+  type    = string
   default = ""
 }
 
@@ -22,31 +22,31 @@ output "tags" {
 
 output "permanent" {
   value = {
-    rg_name  = "spetlr-permanent"
-    resource_name = "spetlrpermanent"
-    metastore_name = "spetlrmetastore"
+    rg_name                    = "spetlr-permanent"
+    resource_name              = "spetlrpermanent"
+    metastore_name             = "spetlrmetastore"
     metastore_admin_group_name = "spetlrmetastoreadmins"
-    cicd_spn_name = "SpetlrGithubPipe"
+    cicd_spn_name              = "SpetlrGithubPipe"
   }
 }
 
 output "integration" {
   value = {
-    rg_name  = "githubspetlr${var.uniqueRunId}"
-    resource_name = "githubspetlr${var.uniqueRunId}"
-    eventhub_name = "spetlreh"
-    kv_secret_db_ws_url = "Databricks--WorkspaceUrl"
+    rg_name                = "spetlr${var.uniqueRunId}"
+    resource_name          = "spetlr${var.uniqueRunId}"
+    eventhub_name          = "spetlreh"
+    kv_secret_db_ws_url    = "Databricks--WorkspaceUrl"
     catalog_container_name = "catalog"
     capture_container_name = "capture"
-    init_container_name = "init"
-    init_drivers_folder = "drivers"
-    pyodbc_driver_name = "msodbcsql18_amd64.deb"
+    init_container_name    = "init"
+    init_drivers_folder    = "drivers"
+    pyodbc_driver_name     = "msodbcsql18_amd64.deb"
     captain = {
-      display_name = "spetrl_captain_${var.uniqueRunId}"
-      kv_secret_id = "Captain--ClientId"
-      kv_secret_pass = "Captain--ClientSecret"
+      display_name     = "spetrl_captain_${var.uniqueRunId}"
+      kv_secret_id     = "Captain--ClientId"
+      kv_secret_pass   = "Captain--ClientSecret"
       kv_secret_tenant = "Captain--TenantId"
-      kv_db_secret = "Captain--DbSecret"
+      kv_db_secret     = "Captain--DbSecret"
     }
   }
 }

624c21675f491054ab393b0b76d08abfd675cab4
Simon Heisterkamp <she@delegate.dk>
2024-10-27 18:26:56 +0000

    fix azure tags

--- src/spetlr/utils/AzureTags.py
+++ src/spetlr/utils/AzureTags.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 
 from spetlr.spark import Spark
 
@@ -6,23 +7,20 @@ from spetlr.spark import Spark
 class AzureTags:
     """Represents easy access to the azure resource tags of the workspace."""
 
-    _tags = None
+    def __init__(self):
+        tags_json = Spark.get().conf.get(
+            "spark.databricks.clusterUsageTags.clusterAllTags"
+        )
+        self._tags: Dict[str, str] = {
+            i["key"]: i["value"] for i in json.loads(tags_json)
+        }
 
-    @classmethod
-    def init(cls):
-        if cls._tags is None:
-            tags_json = Spark.get().conf.get(
-                "spark.databricks.clusterUsageTags.clusterAllTags"
-            )
-            cls._tags = {i["key"]: i["value"] for i in json.loads(tags_json)}
+    def keys(self):
+        return self._tags.keys()
 
-    @classmethod
-    def keys(cls):
-        cls.init()
-        return cls._tags.keys()
-
-    @classmethod
-    def __getattr__(cls, attr):
+    def __getattr__(self, attr) -> str:
         # only called when self.attr doesn't exist
-        cls.init()
-        return cls._tags[attr]
+        return self._tags[attr]
+
+    def asDict(self) -> Dict[str, str]:
+        return self._tags.copy()

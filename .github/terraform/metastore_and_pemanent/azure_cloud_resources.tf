049f9634c2690d0bf5f961c559c65402ccbec6f4
Simon Heisterkamp <she@delegate.dk>
2024-10-28 07:39:46 +0000

    add an admin workspace

--- .github/terraform/metastore_and_pemanent/azure_cloud_resources.tf
+++ .github/terraform/metastore_and_pemanent/azure_cloud_resources.tf
@@ -34,3 +34,10 @@ resource "azurerm_cosmosdb_account" "cosmos_db" {
   }
   depends_on = [azurerm_resource_group.rg]
 }
+
+resource "azurerm_databricks_workspace" "admin" {
+  location            = azurerm_resource_group.rg.location
+  name                = "spetlradmin"
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "premium"
+}

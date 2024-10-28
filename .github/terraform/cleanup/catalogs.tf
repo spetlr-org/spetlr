a0f12af8fa725076b068643b181af78fc7d02ff3
Simon Heisterkamp <she@delegate.dk>
2024-10-28 22:58:02 +0000

    don't assign in terraform

--- .github/terraform/cleanup/catalogs.tf
+++ .github/terraform/cleanup/catalogs.tf
@@ -3,18 +3,6 @@ data "databricks_catalogs" "all" {
   depends_on = [databricks_metastore_assignment.db_metastore_assign_workspace]
 }
 
-
-
-resource "databricks_workspace_binding" "integration" {
-  provider = databricks.ws
-
-  for_each = toset([for each in data.databricks_catalogs.all.ids : each if regex("spetlr[0-9]+", each)])
-
-  securable_name = each.value
-  workspace_id   = data.azurerm_databricks_workspace.admin.workspace_id
-  binding_type   = "BINDING_TYPE_READ_WRITE"
-}
-
 output "catalogs" {
-  value = toset([for each in databricks_workspace_binding.integration : each.securable_name])
+  value = toset([for each in data.databricks_catalogs.all.ids : each if regex("spetlr[0-9]+", each)])
 }

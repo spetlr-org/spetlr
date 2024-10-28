c57672194c592c2d4c90d57be8b5e9fb1437f5a8
Simon Heisterkamp <she@delegate.dk>
2024-10-28 07:35:34 +0000

    clean up disused files

--- .github/deploy/Utilities/Deploy/all.ps1
+++
@@ -1,8 +0,0 @@
-. "$PSScriptRoot\Throw-WhenError.ps1"
-. "$PSScriptRoot\Ignore-Errors.ps1"
-. "$PSScriptRoot\Convert-Safe-FromJson.ps1"
-. "$PSScriptRoot\Generate-Password.ps1"
-. "$PSScriptRoot\Scramble-String.ps1"
-. "$PSScriptRoot\Get-RandomCharacters.ps1"
-. "$PSScriptRoot\Get-KeyVaultSecret.ps1"
-. "$PSScriptRoot\Set-KeyVaultSecret.ps1"

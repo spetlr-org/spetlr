9a7e38c07400e8e99cb070d6a6dda20575e85ae9
Radek Buczkowski <radek.buczkowski.2017@gmail.com>
2024-11-22 08:33:44 +0100

    Black

--- src/spetlr/spark.py
+++ src/spetlr/spark.py
@@ -88,9 +88,12 @@ class Spark:
                         spark.conf.set(key, value)
                 return spark
             except ImportError:
-                raise ValueError("The 'databricks-connect' library is not installed.\n"
-                                 "Install the library or set the 'SPETLR_DATABRICKS_CONNECT'\n"
-                                 "environment variable to 'False'.") from None
+                raise ValueError(
+                    "The 'databricks-connect' library is not installed.\n"
+                    "Install the library or set the 'SPETLR_DATABRICKS_CONNECT'\n"
+                    "environment variable to 'False'."
+                ) from None
+
         return None
 
     @classmethod

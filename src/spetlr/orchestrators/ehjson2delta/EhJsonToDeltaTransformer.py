b68ebcb1ec7547737e1d0bafec497001dd6434e4
Lau Johansson <exljo@clever.dk>
2026-02-13 12:21:38 +0100

    fix: make if-else more explicit

--- src/spetlr/orchestrators/ehjson2delta/EhJsonToDeltaTransformer.py
+++ src/spetlr/orchestrators/ehjson2delta/EhJsonToDeltaTransformer.py
@@ -49,9 +49,10 @@ class EhJsonToDeltaTransformer(Transformer):
             else:
                 df = df.select(*direct_cols)
         else:
-            json_options = {}
-            if not self.case_sensitive:
-                json_options["readerCaseSensitive"] = False
+            if self.case_sensitive:
+                json_options = {"readerCaseSensitive": "true"}
+            else:
+                json_options = {"readerCaseSensitive": "false"}
 
             # every column that is in the target delta table and that is not a direct
             # column from the source eventhub DataFrame, is assumed to be a column whose
